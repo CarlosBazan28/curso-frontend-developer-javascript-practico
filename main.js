54550644b0dd69a1ab9e3f85eadb692351e63dc8
Carlos Bazan <carlosbazanguerra@gmail.com>
2023-06-24 14:05:59 -0500

    mobile-menu and news changes

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email'); //vincular elemento con una variable
 const desktopMenu = document.querySelector('.desktop-menu'); //vincular elemento con una variable
+const burgerMenuIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu); //asignar el evento a ejecutar con la funcion a ejecutar
+burgerMenuIcon.addEventListener('click', toggleMobileMenu); 
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive'); //quitar o poner clase
 }
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+}
+
