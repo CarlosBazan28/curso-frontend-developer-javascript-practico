da245fe3ed888194b8d56b4464cb9e583d91695e
Carlos Bazan <carlosbazanguerra@gmail.com>
2023-06-24 15:06:18 -0500

    menu logic improved

--- main.js
+++ main.js
@@ -2,15 +2,29 @@ const menuEmail = document.querySelector('.navbar-email'); //vincular elemento c
 const desktopMenu = document.querySelector('.desktop-menu'); //vincular elemento con una variable
 const burgerMenuIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const carritoIcon = document.querySelector('.navbar-shopping-cart'); 
+const productDetail = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu); //asignar el evento a ejecutar con la funcion a ejecutar
 burgerMenuIcon.addEventListener('click', toggleMobileMenu); 
+carritoIcon.addEventListener('click', toggleProductDetail);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive'); //quitar o poner clase
+    productDetail.classList.add('inactive');
 }
 
 function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive');
+   mobileMenu.classList.toggle('inactive');
+   productDetail.classList.add('inactive');
+ 
 }
+
+function  toggleProductDetail(){
+    productDetail.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+}
+
+
 
